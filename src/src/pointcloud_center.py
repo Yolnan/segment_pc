Updated centroid calculation to fix index error
Handles case when no object is detected

src/src/pointcloud_center.py
@@ -6,27 +6,29 @@ from segment_pointcloud.msg import obj_centroid_data, obj_pointcloud
 import ros_numpy
 import numpy as np
 
+pub = rospy.Publisher('/pcl_centers',obj_centroid_data,queue_size=1)
 
- 
 def callback(data):
-  pub = rospy.Publisher('/pcl_centers',obj_centroid_data,queue_size=1)
-  xyz = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(data,remove_nans=True)
-  xyz = xyz[xyz[:,2]<np.quantile(xyz[:,2],0.99,0),:]
-  xyz = xyz[xyz[:,2]>np.quantile(xyz[:,2],0.01,0),:]
-  xyz_mean = np.mean(xyz,axis=0)
+  try:
+    xyz = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(data.pcl,remove_nans=True)
+    xyz = xyz[xyz[:,2]<np.quantile(xyz[:,2],0.99,0),:]
+    xyz = xyz[xyz[:,2]>np.quantile(xyz[:,2],0.01,0),:]
+    xyz_mean = np.mean(xyz,axis=0)
 
-  out = obj_centroid_data()
-  if not rospy.is_shutdown():
-    out.header.stamp = rospy.Time.now()
-    out.header.frame_id = "theia/front_camera_aligned_depth_to_color_frame"
-    out.centroid.x = xyz_mean[0]
-    out.centroid.y = xyz_mean[1]
-    out.centroid.z = xyz_mean[2]
-    out.label = data.label
-    out.probability = data.probability
-    out.x_size = data.x_size
-    out.y_size = data.y_size
-    pub.publish(out)
+    out = obj_centroid_data()
+    if not rospy.is_shutdown():
+      out.header.stamp = rospy.Time.now()
+      out.header.frame_id = "theia/front_camera_aligned_depth_to_color_frame"
+      out.centroid.x = xyz_mean[0]
+      out.centroid.y = xyz_mean[1]
+      out.centroid.z = xyz_mean[2]
+      out.label = data.label
+      out.probability = data.probability
+      out.x_size = data.x_size
+      out.y_size = data.y_size
+      pub.publish(out)
+  except IndexError:
+    print(data)
 
 
   